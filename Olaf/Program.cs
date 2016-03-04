525243e2ff6762dfb8130a3ced933c71c10754b9
spudgy <asurabro@gmail.com>
2016-03-04 06:40:23 +0300

    adasdafasdsa

--- Olaf/Program.cs
+++ Olaf/Program.cs
@@ -253,7 +253,7 @@ namespace Olaf
                 return;
             }
             var predq = Q.GetPrediction(Sender);
-            if (Sender.IsValidTarget(Q.Range) && Q.IsReady())
+            if (Sender.IsValidTarget(Q.Range) && Q.IsReady() && !Sender.IsAlly)
             {
                 Q.Cast(predq.CastPosition);
             }
