8d00a06a6fcbc0c580699d346c0dc5bcba423ef6
spudgy <asurabro@gmail.com>
2016-03-23 02:53:24 +0300

    small changes

--- AutoSteal/AutoSteal/Library/Spells/Spells.cs
+++ AutoSteal/AutoSteal/Library/Spells/Spells.cs
@@ -1197,7 +1197,7 @@ namespace GenesisSpellLibrary.Spells
             this.Q = new Spell.Targeted(SpellSlot.Q, 675);
             this.W = new Spell.Skillshot(SpellSlot.W, 1000, SkillShotType.Linear, 250, 1600, 80);
             this.E = new Spell.Skillshot(SpellSlot.E, 475, SkillShotType.Linear);
-            this.R = new Spell.Skillshot(SpellSlot.R, 1400, SkillShotType.Linear, 500, 2800, 110);
+            this.R = new Spell.Skillshot(SpellSlot.R, 1100, SkillShotType.Linear, 500, 2800, 110);
         }
     }
 
--- KappaUtility/KappaUtility/Summoners/Smite.cs
+++ KappaUtility/KappaUtility/Summoners/Smite.cs
@@ -10,65 +10,67 @@
     {
         public static void Smiteopepi()
         {
-            if (Spells.Smite != null)
+            if (Spells.Smite == null)
             {
-                var smitemob = Spells.SummMenu["smitemob"].Cast<CheckBox>().CurrentValue && Spells.Smite.IsReady();
-                var smitecombo = Spells.SummMenu["smitecombo"].Cast<CheckBox>().CurrentValue && Spells.Smite.IsReady();
-                var smiteks = Spells.SummMenu["smiteks"].Cast<CheckBox>().CurrentValue && Spells.Smite.IsReady();
+                return;
+            }
 
-                foreach (var mob in
-                    ObjectManager.Get<Obj_AI_Minion>()
-                        .Where(
-                            jmob =>
-                            !jmob.HasBuffOfType(BuffType.Invulnerability) && jmob.IsHPBarRendered && jmob.IsMonster
-                            && jmob.IsVisible && !jmob.IsDead && !jmob.IsZombie
-                            && ((Spells.SummMenu["drake"].Cast<CheckBox>().CurrentValue
-                                 && jmob.BaseSkinName == "SRU_Dragon")
-                                || (Spells.SummMenu["baron"].Cast<CheckBox>().CurrentValue
-                                    && jmob.BaseSkinName == "SRU_Baron")
-                                || (Spells.SummMenu["gromp"].Cast<CheckBox>().CurrentValue
-                                    && jmob.BaseSkinName == "SRU_Gromp")
-                                || (Spells.SummMenu["krug"].Cast<CheckBox>().CurrentValue
-                                    && jmob.BaseSkinName == "SRU_Krug")
-                                || (Spells.SummMenu["razorbeak"].Cast<CheckBox>().CurrentValue
-                                    && jmob.BaseSkinName == "SRU_Razorbeak")
-                                || (Spells.SummMenu["crab"].Cast<CheckBox>().CurrentValue
-                                    && jmob.BaseSkinName == "Sru_Crab")
-                                || (Spells.SummMenu["murkwolf"].Cast<CheckBox>().CurrentValue
-                                    && jmob.BaseSkinName == "SRU_Murkwolf")
-                                || (Spells.SummMenu["blue"].Cast<CheckBox>().CurrentValue
-                                    && jmob.BaseSkinName == "SRU_Blue")
-                                || (Spells.SummMenu["red"].Cast<CheckBox>().CurrentValue
-                                    && jmob.BaseSkinName == "SRU_Red")))
-                        .Where(mob => smitemob)
-                        .Where(
-                            mob =>
-                            Player.Instance.GetSummonerSpellDamage(mob, DamageLibrary.SummonerSpells.Smite)
-                            >= mob.Health))
+            var smitemob = Spells.SummMenu["smitemob"].Cast<CheckBox>().CurrentValue && Spells.Smite.IsReady();
+            var smitecombo = Spells.SummMenu["smitecombo"].Cast<CheckBox>().CurrentValue && Spells.Smite.IsReady();
+            var smiteks = Spells.SummMenu["smiteks"].Cast<CheckBox>().CurrentValue && Spells.Smite.IsReady();
+
+            foreach (var mob in
+                ObjectManager.Get<Obj_AI_Minion>()
+                    .Where(
+                        jmob =>
+                        !jmob.HasBuffOfType(BuffType.Invulnerability) && jmob.IsHPBarRendered && jmob.IsMonster
+                        && jmob.IsVisible && !jmob.IsDead && !jmob.IsZombie
+                        && ((Spells.SummMenu["drake"].Cast<CheckBox>().CurrentValue
+                             && jmob.BaseSkinName == "SRU_Dragon")
+                            || (Spells.SummMenu["baron"].Cast<CheckBox>().CurrentValue
+                                && jmob.BaseSkinName == "SRU_Baron")
+                            || (Spells.SummMenu["gromp"].Cast<CheckBox>().CurrentValue
+                                && jmob.BaseSkinName == "SRU_Gromp")
+                            || (Spells.SummMenu["krug"].Cast<CheckBox>().CurrentValue
+                                && jmob.BaseSkinName == "SRU_Krug")
+                            || (Spells.SummMenu["razorbeak"].Cast<CheckBox>().CurrentValue
+                                && jmob.BaseSkinName == "SRU_Razorbeak")
+                            || (Spells.SummMenu["crab"].Cast<CheckBox>().CurrentValue
+                                && jmob.BaseSkinName == "Sru_Crab")
+                            || (Spells.SummMenu["murkwolf"].Cast<CheckBox>().CurrentValue
+                                && jmob.BaseSkinName == "SRU_Murkwolf")
+                            || (Spells.SummMenu["blue"].Cast<CheckBox>().CurrentValue
+                                && jmob.BaseSkinName == "SRU_Blue")
+                            || (Spells.SummMenu["red"].Cast<CheckBox>().CurrentValue
+                                && jmob.BaseSkinName == "SRU_Red")))
+                    .Where(mob => smitemob)
+                    .Where(
+                        mob =>
+                        Player.Instance.GetSummonerSpellDamage(mob, DamageLibrary.SummonerSpells.Smite)
+                        >= mob.Health))
+            {
+                Spells.Smite.Cast(mob);
+            }
+
+            foreach (var target in
+                ObjectManager.Get<AIHeroClient>()
+                    .Where(
+                        hero =>
+                        hero != null && hero.IsHPBarRendered && !hero.HasBuffOfType(BuffType.Invulnerability)
+                        && hero.IsValid && hero.IsVisible && hero.IsEnemy && !hero.IsDead && !hero.IsZombie
+                        && !Spells.SummMenu["DontSmite" + hero.BaseSkinName].Cast<CheckBox>().CurrentValue)
+                    .Where(target => target.IsValidTarget(Spells.Smite.Range)))
+            {
+                if (smitecombo)
                 {
-                    Spells.Smite.Cast(mob);
+                    Spells.Smite.Cast(target);
                 }
 
-                foreach (var target in
-                    ObjectManager.Get<AIHeroClient>()
-                        .Where(
-                            hero =>
-                            hero != null && hero.IsHPBarRendered && !hero.HasBuffOfType(BuffType.Invulnerability)
-                            && hero.IsValid && hero.IsVisible && hero.IsEnemy && !hero.IsDead && !hero.IsZombie
-                            && !Spells.SummMenu["DontSmite" + hero.BaseSkinName].Cast<CheckBox>().CurrentValue)
-                        .Where(target => target.IsValidTarget(Spells.Smite.Range)))
+                if (smiteks
+                    && Player.Instance.GetSummonerSpellDamage(target, DamageLibrary.SummonerSpells.Smite)
+                    > target.TotalShieldHealth())
                 {
-                    if (smitecombo)
-                    {
-                        Spells.Smite.Cast(target);
-                    }
-
-                    if (smiteks
-                        && Player.Instance.GetSummonerSpellDamage(target, DamageLibrary.SummonerSpells.Smite)
-                        > target.TotalShieldHealth())
-                    {
-                        Spells.Smite.Cast(target);
-                    }
+                    Spells.Smite.Cast(target);
                 }
             }
         }
