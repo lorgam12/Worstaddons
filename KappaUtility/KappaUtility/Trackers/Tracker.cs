210c2ca39dc8414c69df9aef5bcb338fb31c1f9b
spudgy <asurabro@gmail.com>
2016-03-23 02:56:28 +0300

    Revert "small changes"
    This reverts commit 8d00a06a6fcbc0c580699d346c0dc5bcba423ef6.

--- KappaUtility/KappaUtility/Trackers/Tracker.cs
+++ KappaUtility/KappaUtility/Trackers/Tracker.cs
@@ -138,61 +138,64 @@
             }
 
             var traps = ObjectManager.Get<Obj_AI_Minion>();
-            foreach (var trap in traps.Where(trap => trap != null))
             {
-                switch (trap.Name)
+                foreach (var trap in traps.Where(trap => trap != null))
                 {
-                    case "Cupcake Trap":
-                        Drawing.DrawText(
-                            Drawing.WorldToScreen(trap.Position) - new Vector2(30, -30),
-                            Color.White,
-                            "Caitlyn Trap",
-                            2);
-                        Circle.Draw(SharpDX.Color.Purple, trap.BoundingRadius + 10, trap.Position);
-                        break;
-
-                    case "Noxious Trap":
-                        if (trap.BaseSkinName == "NidaleeSpear")
-                        {
-                            var endTime = Math.Max(0, -Game.Time + 120);
+                    switch (trap.Name)
+                    {
+                        case "Cupcake Trap":
                             Drawing.DrawText(
                                 Drawing.WorldToScreen(trap.Position) - new Vector2(30, -30),
                                 Color.White,
-                                "Nidalee Trap",
+                                "Caitlyn Trap",
                                 2);
-                            Circle.Draw(SharpDX.Color.Green, trap.BoundingRadius + 25, trap.Position);
-                        }
+                            Circle.Draw(SharpDX.Color.Purple, trap.BoundingRadius + 10, trap.Position);
+                            break;
 
-                        if (trap.BaseSkinName == "TeemoMushroom")
-                        {
-                            if (trap.GetBuff("BantamTrap") != null)
+                        case "Noxious Trap":
+                            if (trap.BaseSkinName == "NidaleeSpear")
                             {
-                                var endTime = Math.Max(0, trap.GetBuff("BantamTrap").EndTime - Game.Time);
+                                var endTime = Math.Max(0, -Game.Time + 120);
                                 Drawing.DrawText(
                                     Drawing.WorldToScreen(trap.Position) - new Vector2(30, -30),
                                     Color.White,
-                                    "Teemo Mushroom Expire: "
-                                    + Convert.ToString(endTime, CultureInfo.InstalledUICulture),
+                                    "Nidalee Trap",
+                                    2);
+                                Circle.Draw(SharpDX.Color.Green, trap.BoundingRadius + 25, trap.Position);
+                            }
+
+                            if (trap.BaseSkinName == "TeemoMushroom")
+                            {
+                                if (trap.GetBuff("BantamTrap") != null)
+                                {
+                                    var endTime = Math.Max(0, trap.GetBuff("BantamTrap").EndTime - Game.Time);
+                                    Drawing.DrawText(
+                                        Drawing.WorldToScreen(trap.Position) - new Vector2(30, -30),
+                                        Color.White,
+                                        "Teemo Mushroom Expire: "
+                                        + Convert.ToString(endTime, CultureInfo.InstalledUICulture),
+                                        2);
+                                }
+
+                                Circle.Draw(SharpDX.Color.Green, trap.BoundingRadius * 3, trap.Position);
+                            }
+                            break;
+
+                        case "Jack In The Box":
+                            if (trap.GetBuff("JackInTheBox") != null)
+                            {
+                                var endTime = Math.Max(0, trap.GetBuff("JackInTheBox").EndTime - Game.Time);
+                                Drawing.DrawText(
+                                    Drawing.WorldToScreen(trap.Position) - new Vector2(30, -30),
+                                    Color.White,
+                                    "Shaco Box Expire: "
+                                    + Convert.ToString(endTime, CultureInfo.InvariantCulture),
                                     2);
                             }
 
-                            Circle.Draw(SharpDX.Color.Green, trap.BoundingRadius * 3, trap.Position);
-                        }
-                        break;
-
-                    case "Jack In The Box":
-                        if (trap.GetBuff("JackInTheBox") != null)
-                        {
-                            var endTime = Math.Max(0, trap.GetBuff("JackInTheBox").EndTime - Game.Time);
-                            Drawing.DrawText(
-                                Drawing.WorldToScreen(trap.Position) - new Vector2(30, -30),
-                                Color.White,
-                                "Shaco Box Expire: " + Convert.ToString(endTime, CultureInfo.InvariantCulture),
-                                2);
-                        }
-
-                        Circle.Draw(SharpDX.Color.Green, trap.BoundingRadius * 15, trap.Position);
-                        break;
+                            Circle.Draw(SharpDX.Color.Green, trap.BoundingRadius * 15, trap.Position);
+                            break;
+                    }
                 }
             }
         }
