7d1852d7a4f82aeb7f93138511efca7aa7d8f057
spudgy <asurabro@gmail.com>
2016-04-18 19:45:49 +0300

    KappAzir

---
+++ KappAzir/KappAzir/Modes/Flee.cs
@@ -0,0 +1,31 @@
+﻿namespace KappAzir.Modes
+{
+    using System.Linq;
+
+    using EloBuddy;
+    using EloBuddy.SDK;
+    
+    using static Menus;
+    using static SpellsManager;
+    using Mario_s_Lib;
+
+    internal class Flee : ModeManager
+    {
+        public static void Execute()
+        {
+            var allready = Q.IsReady() && E.IsReady() && W.IsReady();
+            if (Orbwalker.AzirSoldiers.Count(s => s.IsAlly) < 1 && allready && ManaCheck(Azir) < Azir.Mana)
+            {
+                W.Cast(Azir.Position.Extend(Game.CursorPos, W.Range).To3D());
+            }
+
+            if (Orbwalker.AzirSoldiers.Count(s => s.IsAlly) > 0)
+            {
+                if (E.Cast(Azir.Position.Extend(Game.CursorPos, E.Range).To3D()))
+                {
+                    Core.DelayAction(() => { Q.Cast(Azir.Position.Extend(Game.CursorPos, Q.Range).To3D()); }, FleeMenu.GetSliderValue("delay"));
+                }
+            }
+        }
+    }
+}
---
+++ KappAzir/KappAzir/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace KappAzir
+{
+    using System;
+
+    using EloBuddy;
+    using EloBuddy.SDK.Events;
+
+    using Modes;
+
+    internal class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        /// <summary>
+        /// The firs thing that runs on the template
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        /// <summary>
+        /// This event is triggered when the game loads
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            try
+            {
+                //Put the name of the champion here
+                if (Player.Instance.ChampionName != "Azir")
+                {
+                    return;
+                }
+
+                SpellsManager.InitializeSpells();
+                Menus.CreateMenu();
+                ModeManager.InitializeModes();
+                DrawingsManager.InitializeDrawings();
+            }
+            catch (Exception e)
+            {
+                if (e.ToString().Contains("Mario"))
+                {
+                    Chat.Print("[KappAzir ERROR] Failed to Load addon Please Make sure you have Mario's Lib installed");
+                    Console.Write("[KappAzir ERROR] Failed to Load addon Please Make sure you have Mario's Lib installed");
+                }
+            }
+        }
+    }
+}
