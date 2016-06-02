4ae431d7fdbe5fabeacfe77fe9c55516c4951800
spudgy <asurabro@gmail.com>
2016-06-03 02:55:37 +0300

    BaseUlt

---
+++ KappaBaseUlt/KappaBaseUlt/Database.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KappaBaseUlt
+{
+    using EloBuddy;
+    using EloBuddy.SDK.Enumerations;
+
+    class Database
+    {
+        internal struct Hero
+        {
+            public Champion Champion;
+
+            public SpellSlot Slot;
+
+            public SkillShotType Type;
+
+            public uint Range;
+
+            public int Width;
+
+            public int CastDelay;
+
+            public int Speed;
+
+            public float AllowedCollisionCount;
+        }
+        internal struct Damage
+        {
+            public Champion Champion;
+
+            public DamageType DamageType;
+            public float[] Floats;
+            public float Float;
+        }
+
+        internal static readonly List<Damage> Damages = new List<Damage>()
+                                                            {
+                                                                new Damage
+                                                                    {
+                                                                        Champion = Champion.Ashe, DamageType = DamageType.Magical, Floats = new float[] { 250, 425, 600 }, Float = 1f
+                                                                    },
+                                                                new Damage
+                                                                    {
+                                                                        Champion = Champion.Draven, DamageType = DamageType.Physical, Floats = new float[] { 175, 275, 375 }, Float = 1.1f
+                                                                    },
+                                                                new Damage
+                                                                    {
+                                                                        Champion = Champion.Ezreal, DamageType = DamageType.Magical, Floats = new float[] { 350, 500, 600 }, Float = 1f
+                                                                    },
+                                                                new Damage
+                                                                    {
+                                                                        Champion = Champion.Lux, DamageType = DamageType.Magical, Floats = new float[] { 300, 400, 500 }, Float = 0.75f
+                                                                    },
+                                                                new Damage
+                                                                    {
+                                                                        Champion = Champion.Karthus, DamageType = DamageType.Magical, Floats = new float[] { 300, 400, 500 }, Float = 0.6f
+                                                                    },
+                                                                new Damage
+                                                                    {
+                                                                        Champion = Champion.Pantheon, DamageType = DamageType.Magical, Floats = new float[] { 200, 350, 500 }, Float = 0.5f
+                                                                    },
+                                                                new Damage
+                                                                    {
+                                                                        Champion = Champion.Gangplank, DamageType = DamageType.Magical, Floats = new float[] { 50, 70, 90 }, Float = 0.1f
+                                                                    },
+                                                                new Damage
+                                                                    {
+                                                                        Champion = Champion.Jinx, DamageType = DamageType.Physical, Floats = new float[] { 25, 35, 45 }, Float = 0.1f
+                                                                    },
+                                                                new Damage
+                                                                    {
+                                                                        Champion = Champion.Ziggs, DamageType = DamageType.Magical, Floats = new float[] { 200, 300, 400 }, Float = 0.73f
+                                                                    }
+                                                            };
+        internal static readonly List<Hero> Champions = new List<Hero>()
+                                                           {
+                                                               new Hero
+                                                                   {
+                                                                       Champion = Champion.Ashe, Type = SkillShotType.Linear, Slot = SpellSlot.R,
+                                                                       AllowedCollisionCount = 0, CastDelay = 250, Speed = 1600, Width = 250,
+                                                                       Range = int.MaxValue
+                                                                   },
+                                                               new Hero
+                                                                   {
+                                                                       Champion = Champion.Draven, Type = SkillShotType.Linear, Slot = SpellSlot.R,
+                                                                       AllowedCollisionCount = int.MaxValue, CastDelay = 300, Speed = 2000, Width = 160,
+                                                                       Range = int.MaxValue
+                                                                   },
+                                                               new Hero
+                                                                   {
+                                                                       Champion = Champion.Ezreal, Type = SkillShotType.Linear, Slot = SpellSlot.R,
+                                                                       AllowedCollisionCount = int.MaxValue, CastDelay = 1000, Speed = 2000, Width = 160,
+                                                                       Range = int.MaxValue
+                                                                   },
+                                                               new Hero
+                                                                   {
+                                                                       Champion = Champion.Karthus, Type = SkillShotType.Circular, Slot = SpellSlot.R,
+                                                                       AllowedCollisionCount = int.MaxValue, CastDelay = 3000, Speed = int.MaxValue,
+                                                                       Width = int.MaxValue, Range = int.MaxValue
+                                                                   },
+                                                               new Hero
+                                                                   {
+                                                                       Champion = Champion.Lux, Type = SkillShotType.Linear, Slot = SpellSlot.R,
+                                                                       AllowedCollisionCount = int.MaxValue, CastDelay = 1000, Speed = int.MaxValue,
+                                                                       Width = 250, Range = 3340
+                                                                   },
+                                                               new Hero
+                                                                   {
+                                                                       Champion = Champion.Pantheon, Type = SkillShotType.Circular, Slot = SpellSlot.R,
+                                                                       AllowedCollisionCount = int.MaxValue, CastDelay = 4700, Speed = int.MaxValue,
+                                                                       Width = 250, Range = 5500
+                                                                   },
+                                                               new Hero
+                                                                   {
+                                                                       Champion = Champion.Gangplank, Type = SkillShotType.Circular, Slot = SpellSlot.R,
+                                                                       AllowedCollisionCount = int.MaxValue, CastDelay = 250, Speed = int.MaxValue,
+                                                                       Width = 700, Range = int.MaxValue
+                                                                   },
+                                                               new Hero
+                                                                   {
+                                                                       Champion = Champion.Jinx, Type = SkillShotType.Linear, Slot = SpellSlot.R,
+                                                                       AllowedCollisionCount = 0, CastDelay = 500, Speed = 2100, Width = 140,
+                                                                       Range = int.MaxValue
+                                                                   },
+                                                               new Hero
+                                                                   {
+                                                                       Champion = Champion.Ziggs, Type = SkillShotType.Circular, Slot = SpellSlot.R,
+                                                                       AllowedCollisionCount = int.MaxValue, CastDelay = 250, Speed = 1500, Width = 500,
+                                                                       Range = 5300
+                                                                   }
+                                                           };
+    }
+}
---
+++ KappaBaseUlt/KappaBaseUlt/Program.cs
@@ -0,0 +1,286 @@
+﻿namespace KappaBaseUlt
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    using EloBuddy;
+    using EloBuddy.SDK;
+    using EloBuddy.SDK.Enumerations;
+    using EloBuddy.SDK.Events;
+    using EloBuddy.SDK.Menu;
+    using EloBuddy.SDK.Menu.Values;
+    using EloBuddy.SDK.Rendering;
+
+    using SharpDX;
+
+    using Color = SharpDX.Color;
+
+    internal static class Program
+    {
+
+        private static Menu baseMenu;
+
+        private static readonly List<EnemyInfo> RecallsList = new List<EnemyInfo>();
+
+        private static Spell.Skillshot R { get; set; }
+
+        private static float Damage;
+
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            var me = Database.Champions.FirstOrDefault(hero => hero.Champion == Player.Instance.Hero);
+            if (me.Champion != Player.Instance.Hero)
+            {
+                return;
+            }
+
+            R = new Spell.Skillshot(me.Slot, me.Range, me.Type, me.CastDelay, me.Speed, me.Width);
+
+            baseMenu = MainMenu.AddMenu("KappaBaseUlt", "KappaBaseUlt");
+            baseMenu.AddGroupLabel(Player.Instance.Hero + " BaseUlt");
+            baseMenu.AddSeparator(0);
+            baseMenu.AddGroupLabel("Key Settings:");
+            baseMenu.Add("enable", new KeyBind("Enable BaseUlt", true, KeyBind.BindTypes.PressToggle, 'K'));
+            baseMenu.Add("disable", new KeyBind("Disable Key", false, KeyBind.BindTypes.HoldActive, 32));
+            baseMenu.AddSeparator(0);
+            baseMenu.AddGroupLabel("Settings:");
+            baseMenu.Add("col", new CheckBox("Check Collison"));
+            baseMenu.Add("limit", new Slider("FoW Time Limit [{0}]", 120, 0, 180));
+            baseMenu.AddLabel("0 = Always");
+            baseMenu.AddSeparator(0);
+            baseMenu.AddGroupLabel("Drawings:");
+            baseMenu.Add("draw", new CheckBox("Draw Debug Drawings"));
+            baseMenu.AddGroupLabel("BaseUlt Enemies:");
+            RecallsList.Clear();
+            foreach (var enemy in EntityManager.Heroes.Enemies)
+            {
+                baseMenu.Add(enemy.NetworkId.ToString(), new CheckBox("Use On " + enemy.BaseSkinName + " - (" + enemy.Name + ")"));
+                RecallsList.Add(new EnemyInfo(enemy));
+            }
+
+            Game.OnTick += Game_OnTick;
+            Teleport.OnTeleport += Teleport_OnTeleport;
+            Drawing.OnDraw += Drawing_OnDraw;
+        }
+
+        private static void Game_OnTick(EventArgs args)
+        {
+            foreach (var enemy in RecallsList.Where(e => e.Enemy.IsHPBarRendered && !e.Enemy.IsDead))
+            {
+                enemy.lastseen = Game.Time;
+            }
+
+            foreach (var enemy in RecallsList.Where(d => baseMenu[d.Enemy.NetworkId.ToString()].Cast<CheckBox>().CurrentValue && d.Duration > 0))
+            {
+                DoBaseUlt(enemy);
+            }
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if(!baseMenu["draw"].Cast<CheckBox>().CurrentValue) return;
+
+            var X = Player.Instance.ServerPosition.WorldToScreen().X;
+            var Y = Player.Instance.ServerPosition.WorldToScreen().Y;
+            
+            foreach (var player in RecallsList.Where(e => baseMenu[e.Enemy.NetworkId.ToString()].Cast<CheckBox>().CurrentValue && e.Duration > 0))
+            {
+                Drawing.DrawText(
+                    X,
+                    Y,
+                    System.Drawing.Color.White,
+                    player.Enemy.BaseSkinName + " | CountDown: " + (player.CountDown()) + " | TravelTime: " + player.Enemy.traveltime()
+                    + " | LastSeen: " + (Game.Time - player.lastseen) + " | Damage: " + (player.Enemy.GetDamage()) + " | Health: "
+                    + player.Enemy.TotalShieldHealth(),
+                    5);
+            }
+            /*
+            var order = ObjectManager.Get<GameObject>().FirstOrDefault(name => name.Name == "__Spawn_T1");
+            var choes = ObjectManager.Get<GameObject>().FirstOrDefault(name => name.Name == "__Spawn_T2");
+            Circle.Draw(Color.White, 100, order);
+            Circle.Draw(Color.White, 100, choes);
+            */
+        }
+
+        private static void Teleport_OnTeleport(Obj_AI_Base sender, Teleport.TeleportEventArgs args)
+        {
+            if (!sender.IsEnemy || args.Type != TeleportType.Recall)
+            {
+                return;
+            }
+
+            if (args.Status == TeleportStatus.Start)
+            {
+                if (RecallsList.Exists(s => s.Enemy.NetworkId.Equals(sender.NetworkId)))
+                {
+                    RecallsList.Add(
+                        new EnemyInfo(sender) { Duration = args.Duration, Started = args.Start, RecallDuration = args.Duration + Core.GameTickCount });
+                }
+                else
+                {
+                    RecallsList.Add(
+                        new EnemyInfo(sender) { Duration = args.Duration, Started = args.Start, RecallDuration = args.Duration + Core.GameTickCount });
+                }
+            }
+            else
+            {
+                var remove = RecallsList.FirstOrDefault(r => r.Enemy.NetworkId.Equals(sender.NetworkId));
+                if (remove == null)
+                {
+                    return;
+                }
+                RecallsList.Remove(remove);
+                removeFromList(remove.Enemy);
+            }
+        }
+
+        private static bool Killable(this Obj_AI_Base target)
+        {
+            return target.GetDamage() >= target.TotalShieldHealth();
+        }
+
+        private static float GetDamage(this Obj_AI_Base target)
+        {
+            if (!R.IsLearned || !R.IsReady())
+            {
+                return 0;
+            }
+
+            var missinghealth = target.MaxHealth - target.Health;
+            var level = R.Level - 1;
+            var hero = Player.Instance.Hero;
+            var AD = Player.Instance.TotalAttackDamage;
+            var AP = Player.Instance.TotalMagicalDamage;
+
+            var champion = Database.Damages.FirstOrDefault(h => h.Champion == hero);
+
+            if (champion.Champion != hero) return 0;
+
+            if (champion.DamageType == DamageType.Magical)
+            {
+                Damage = champion.Floats[level] + champion.Float * AP;
+            }
+
+            if (champion.DamageType == DamageType.Physical)
+            {
+                Damage = champion.Floats[level] + champion.Float * AD;
+            }
+
+            if (champion.Champion == Champion.Ezreal)
+            {
+                Damage = champion.Floats[level] + (1f * AD + (0.9f * AP));
+            }
+
+            if (champion.Champion == Champion.Gangplank)
+            {
+                Damage = (champion.Floats[level] + champion.Float * AP) * 3;
+            }
+
+            if (champion.Champion == Champion.Jinx)
+            {
+                Damage = champion.Floats[level] + new[] { 0.25f * missinghealth, 0.30f * missinghealth, 0.35f * missinghealth }[level]
+                         + (0.1f * AD);
+            }
+
+            return Player.Instance.CalculateDamageOnUnit(target, champion.DamageType, Damage);
+        }
+
+        private static void removeFromList(Obj_AI_Base sender)
+        {
+            var recall = RecallsList.FirstOrDefault(x => x.Enemy.NetworkId.Equals(sender.NetworkId));
+
+            if (recall == null)
+            {
+                return;
+            }
+
+            RecallsList.Remove(recall);
+        }
+
+        private static float traveltime(this Obj_AI_Base target)
+        {
+            var hero = Player.Instance.Hero;
+            var pos = target.Fountain();
+            var distance = Player.Instance.Distance(pos);
+            var speed = R.Speed;
+
+            if (hero == Champion.Lux || hero == Champion.Karthus || hero == Champion.Pantheon || hero == Champion.Gangplank)
+            {
+                return R.CastDelay;
+            }
+
+            return ((distance / speed) * 1000f) + R.CastDelay;
+        }
+
+        private static void DoBaseUlt(EnemyInfo target)
+        {
+            var disable = baseMenu["disable"].Cast<KeyBind>().CurrentValue;
+            var enable = baseMenu["enable"].Cast<KeyBind>().CurrentValue;
+            float CountDown = target.CountDown();
+            float Traveltime = traveltime(target.Enemy);
+
+            if (enable && !disable)
+            {
+                if (lastseen(target) && CountDown >= Traveltime && target.Enemy.Killable())
+                {
+                    if (CountDown - Traveltime < 60 && !target.Enemy.Collison())
+                    {
+                        Player.CastSpell(R.Slot, target.Enemy.Fountain());
+                    }
+                }
+            }
+        }
+
+        private static bool lastseen(EnemyInfo target)
+        {
+            float timelimit = baseMenu["limit"].Cast<Slider>().CurrentValue;
+            return Game.Time - target.lastseen < timelimit;
+        }
+
+        private static float CountDown(this EnemyInfo target)
+        {
+            return target.Started + target.Duration - Core.GameTickCount;
+        }
+
+        private static bool Collison(this Obj_AI_Base target)
+        {
+            var col = baseMenu["col"].Cast<CheckBox>().CurrentValue;
+            var Rectangle = new Geometry.Polygon.Rectangle(Player.Instance.ServerPosition, target.Fountain(), R.Width);
+
+            return col && EntityManager.Heroes.Enemies.Count(e => Rectangle.IsInside(e) && e.IsValidTarget()) > R.AllowedCollisionCount;
+        }
+
+        private static Vector3 Fountain(this Obj_AI_Base target)
+        {
+            var objSpawnPoint = ObjectManager.Get<Obj_SpawnPoint>().FirstOrDefault(x => x.Team == target.Team);
+            return objSpawnPoint?.Position ?? Vector3.Zero;
+        }
+
+        public class EnemyInfo
+        {
+            public Obj_AI_Base Enemy;
+
+            public float lastseen;
+
+            public float RecallDuration;
+
+            public float Duration;
+
+            public float Started;
+
+            public EnemyInfo(Obj_AI_Base enemy)
+            {
+                this.Enemy = enemy;
+                this.lastseen = 0f;
+                this.Duration = 0f;
+            }
+        }
+    }
+}
